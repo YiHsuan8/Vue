cd35f98a7b1656bddae3dd13cffca1152d7aeada
ADAM TSAI <104122054+YiHsuan8@users.noreply.github.com>
2022-12-24 00:05:32 +0800

    Update AboutView.vue

--- test_vue_cli/src/views/AboutView.vue
+++ test_vue_cli/src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+      <Carousel v-model="value" loop>
+          <CarouselItem v-for="item in 5" key="item">
+              <img :src="require('@/assets/logo.png')" alt="" />
+          </CarouselItem>
+      </Carousel>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+      return {
+          value: 0,
+      };
+  },
+};
+</script>
+<style lang="sass"></style>
