2bb676d803cf57c62b49ab1db9fd9d56e3466027
ADAM TSAI <104122054+YiHsuan8@users.noreply.github.com>
2022-12-24 00:06:04 +0800

    Update HomeView.vue

--- test_vue_cli/src/views/HomeView.vue
+++ test_vue_cli/src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld title="koala" OOXX="testOOXX" msg="1" />
+    <HelloWorld title="koalaB" msg="2" />
+    <HelloWorld title="koalaC" msg="3" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style scoped lang="scss">
+.testOOXX {
+  background-color: rgb(96, 14, 211);
+}
+</style>
