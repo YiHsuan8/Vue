796f8e8783a01652f3f2829dc892646e632ac90f
ADAM TSAI <104122054+YiHsuan8@users.noreply.github.com>
2022-12-24 00:18:12 +0800

    Update App.vue

--- test_vue_cli/src/App.vue
+++ test_vue_cli/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/News">News</router-link> |
+      <router-link to="/product">product</router-link>
   </nav>
   <router-view/>
 </template>
