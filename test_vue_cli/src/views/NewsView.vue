a35a02cbb32b28262e3df6d38fd450c743eade6e
ADAM TSAI <104122054+YiHsuan8@users.noreply.github.com>
2022-12-24 00:17:26 +0800

    Create NewsView.vue

---
+++ test_vue_cli/src/views/NewsView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="news">
+        <p>news</p>
+        <div></div>
+
+        <Images link="https://fakeimg.pl/100" />
+
+        <div>
+            <img alt="vuelogo" :src="require('@/assets/logo.png')" />
+        </div>
+        <!-- <div class="img imgwrap" v-for="item in 6" key="item">
+            <img alt="Vue logo" :src="require('@/assets/logo.png')" />
+        </div> -->
+        <!-- <Carousel v-model="value" loop>
+            <CarouselItem v-for="item in 5" key="item">
+                <img :src="require('@/assets/index/carousel-01.png')" alt="" />
+            </CarouselItem>
+        </Carousel> -->
+    </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+import Images from "@/components/Images.vue";
+
+export default {
+    name: "newsView",
+    components: {
+        HelloWorld,
+        Images,
+    },
+};
+</script>
